24b20531975ed6264067b6979c21e970642094cb
Breno Froes <breno_froes@id.uff.br>
2021-06-22 20:42:56 -0300

    Test failed 2

--- boleto.py
+++ boleto.py
@@ -1,7 +1,8 @@
 import unittest
 
 class Boleto:
-    def __init__(self, codigoBoleto, valorPago, data):
+    def __init__(self, codigoBoleto, valorPago, data, pagamento):
         self.codigoBoleto = codigoBoleto
         self.valorPago = valorPago
         self.data = data
+        self.pagamento = pagamento
--- processador.py
+++ processador.py
@@ -1,12 +1,14 @@
-import unittest
-from fatura import Fatura
 from boleto import Boleto
+from fatura import Fatura
+import unittest
 
 class Processador(unittest.TestCase):
+    def teste_processador(self, boletos):
+        boletos = []
 
     def teste_inicial(self):
         fatura = Fatura(0, '', '')
-        boleto = Boleto(0, 0, '')
+        boleto = Boleto(0, 0, '', '')
         if boleto == fatura:
             return True
         return False
